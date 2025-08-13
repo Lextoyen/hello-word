b5f45e5ba286931ed2ba2e3470e887d3c156629b
Lextoyen <159099977+Lextoyen@users.noreply.github.com>
2025-08-13 17:37:31 +0700

    Update style.js

--- style.js
+++ style.js
@@ -170,7 +170,7 @@
             message += `\nລວມທັງໝົດ: ${formatPrice(total)}KIP\n\nຂໍຂອບໃຈ`;
             
             const encodedMessage = encodeURIComponent(message);
-            const whatsappUrl = `https://wa.me/85697272081?text=${encodedMessage}`;
+            const whatsappUrl = `https://wa.me/8562097272081?text=${encodedMessage}`;
             window.open(whatsappUrl, '_blank');
         }
 
@@ -187,4 +187,5 @@
             showPage('home');
 
         });
+
 
